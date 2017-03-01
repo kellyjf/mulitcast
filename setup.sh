40b49a468a6b97fc716b9f606d9d449ba8a7facc
John Kelly <jkelly@aircell.com>
2017-03-01 16:08:22 -0700

    Add mrouted/pimd switch

--- setup.sh
+++ setup.sh
@@ -1,53 +1,67 @@
 #!/bin/bash
 
+mproto="mrouted"
+mproto="pimd"
 
 stem="net"
 fanout=2
-depth=2
 
-function recurse {
+function buildns {
 	local base=$1
 	local level=$2
+	local cnt=$3
+	
+	$STEP ip netns del ${stem}${base}${cnt} &> /dev/null
+	$STEP ip netns add ${stem}${base}${cnt}
+
+	$STEP ip netns exec ${stem}${base}${cnt} ip link set lo up
+	$STEP ip link add down-${base}${cnt} link edown type vlan id ${base}${cnt}
+	$STEP ip link set down-${base}${cnt} netns ${stem}${base} up
+
+	$STEP ip link add up-${base}${cnt} link eup type vlan id ${base}${cnt}
+	$STEP ip link set up-${base}${cnt} netns ${stem}${base}${cnt} up
+	$STEP ip netns exec ${stem}${base}${cnt} ip addr add 192.168.${base}${cnt}.2/30 dev up-${base}${cnt}
+	$STEP ip netns exec ${stem}${base} ip addr add 192.168.${base}${cnt}.1/30 dev down-${base}${cnt}
+	$STEP ip netns exec ${stem}${base}${cnt} ip route add default via 192.168.${base}${cnt}.1 dev up-${base}${cnt}
+
+
+
+
+
+	$STEP echo -e "hostname $stem$base$cnt\npassword zebra\nlog stdout\nrouter rip\n" >/tmp/ripd-${base}${cnt}
+	$STEP echo -e " network up-${base}${cnt}\n" >>/tmp/ripd-${base}${cnt}
+	$STEP echo -e " network down-${base}${cnt}\n" >>/tmp/ripd-${base}
+	$STEP cp zebra.conf /tmp/zebra-${base}${cnt}
+	$STEP cp pimd.conf /tmp/pimd-${base}${cnt}
+	$STEP cp mrouted.conf /tmp/mrouted-${base}${cnt}
+
+}
+
+function routers {
+	local base=$1
+	local level=$2
+	local cnt=$3
+	
+	$STEP ip netns exec ${stem}${base}${cnt} zebra -d -f /tmp/zebra-${base}${cnt} -i /tmp/zebra-pid-${base}${cnt}
+	$STEP ip netns exec ${stem}${base}${cnt} ripd -d -f /tmp/ripd-${base}${cnt} -i /tmp/ripd-pid-${base}${cnt}
+	$STEP ip netns exec ${stem}${base}${cnt} ./p${mproto} ${base}${cnt} 
+}
+
+function recurse {
+	local cmd=$1
+	local depth=$2
+	local base=$3
+	local level=$4
 	local cnt=0
 	
 	if [ $level -gt $depth ] ; then return ; fi
 
 	for cnt in $(seq $fanout); do
+		"$cmd" "$base" "$level" "$cnt"	
 
-		$STEP ip netns del ${stem}${base}${cnt} &> /dev/null
-		$STEP ip netns add ${stem}${base}${cnt}
-
-		$STEP ip netns exec ${stem}${base}${cnt} ip link set lo up
-		$STEP ip link add down-${base}${cnt} link edown type vlan id ${base}${cnt}
-		$STEP ip link set down-${base}${cnt} netns ${stem}${base} up
- 
-		$STEP ip link add up-${base}${cnt} link eup type vlan id ${base}${cnt}
-		$STEP ip link set up-${base}${cnt} netns ${stem}${base}${cnt} up
-		$STEP ip netns exec ${stem}${base}${cnt} ip addr add 192.168.${base}${cnt}.2/30 dev up-${base}${cnt}
-		$STEP ip netns exec ${stem}${base} ip addr add 192.168.${base}${cnt}.1/30 dev down-${base}${cnt}
-		$STEP ip netns exec ${stem}${base}${cnt} ip route add default via 192.168.${base}${cnt}.1 dev up-${base}${cnt}
-
-
-
-
-		$STEP echo -e "hostname $stem$base$cnt\npassword zebra\nlog stdout\nrouter rip\n" >/tmp/ripd-${base}${cnt}
-		$STEP echo -e " network up-${base}${cnt}\n" >>/tmp/ripd-${base}${cnt}
-		$STEP echo -e " network down-${base}${cnt}\n" >>/tmp/ripd-${base}
-		$STEP cp zebra.conf /tmp/zebra-${base}${cnt}
-		$STEP cp pimd.conf /tmp/pimd-${base}${cnt}
-		$STEP cp mrouted.conf /tmp/mrouted-${base}${cnt}
-
-
-
-		echo "BASE CALL $base $cnt $level"
-		recurse "${base}${cnt}" $(($level+1))
-		echo "BASE RETN $base $cnt $level"
-
-		if [ $level -lt $depth ];  then
-			$STEP ip netns exec ${stem}${base}${cnt} zebra -d -f /tmp/zebra-${base}${cnt} -i /tmp/zebra-pid-${base}${cnt}
-			$STEP ip netns exec ${stem}${base}${cnt} ripd -d -f /tmp/ripd-${base}${cnt} -i /tmp/ripd-pid-${base}${cnt}
-			$STEP ip netns exec ${stem}${base}${cnt} ./pmrouted ${base}${cnt} 
-		fi
+		echo "BASE CALL $cmd $depth $base $cnt $level"
+		recurse "$cmd" "$depth" "${base}${cnt}" "$(($level+1))"
+		echo "BASE RETN $cmd $depth $base $cnt $level"
 
 	done
 }
@@ -70,15 +84,18 @@ function stop_sim {
 }
 
 
-stop_sim
 
 
 if [ $# -eq 0 ]; then
-STEP=echo
-exit
+	stop_sim
+	exit
+else
+	case "$1" in
+		p*) mproto=pimd ;;
+		m*) mproto=mrouted ;;
+		*)  STEP=echo;;
+	esac
 fi
-
-
 
 
 
@@ -90,7 +107,7 @@ $STEP ip netns add $stem
 $STEP ip link add $stem-up link eup type vlan id 777
 $STEP ip link add $stem-down link edown type vlan id 777
 
-$STEP ip link set $stem-up netns  $stem  up
+$STEP ip link set $stem-up netns  $stem multicast off  up
 $STEP ip link set $stem-down up
 $STEP ip addr add 192.168.77.1/30 dev $stem-down
 
@@ -102,7 +119,8 @@ $STEP ip netns exec $stem iptables -t nat -D POSTROUTING -o $stem-up -j MASQUERA
 $STEP cp zebra.conf /tmp/zebra-
 $STEP echo -e "hostname $stem$base$cnt\npassword zebra\nlog stdout\nrouter rip\n" >/tmp/ripd-
 
-recurse ""  0 
+recurse buildns 2 ""  0 
+recurse routers 1 ""  0 
 
 $STEP ip netns exec $stem ip route add 224.0.0.0/4 dev down-1 metric 1
 $STEP ip netns exec $stem ip route add 224.0.0.0/4 dev down-2 metric 2
